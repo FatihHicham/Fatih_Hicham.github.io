b2eab99e37a4c5957df169224c00e64fe41529ac
FatihHicham <fatihhicham0@gmail.com>
2020-03-19 21:08:01 +0100

    Updates

--- precache-manifest.bc32c9d049c833fa2f6036322b6e15b4.js
+++ precache-manifest.bc32c9d049c833fa2f6036322b6e15b4.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4261432a8c52c40d89a8d4dff5036bd0",
+    "revision": "e425586fe35815437622123db9d7eb5d",
     "url": "/MyPortfolio/index.html"
   },
   {
-    "revision": "115e6bd4248cbe892fd0",
+    "revision": "5a202456c798cc82c1ec",
     "url": "/MyPortfolio/static/css/2.559fd73f.chunk.css"
   },
   {
-    "revision": "3b5713cc15067b02c656",
+    "revision": "298eff60e1cfd6809c70",
     "url": "/MyPortfolio/static/css/main.d91305d1.chunk.css"
   },
   {
-    "revision": "115e6bd4248cbe892fd0",
-    "url": "/MyPortfolio/static/js/2.1e8dce61.chunk.js"
+    "revision": "5a202456c798cc82c1ec",
+    "url": "/MyPortfolio/static/js/2.e512ea41.chunk.js"
   },
   {
     "revision": "c7abd4a823ee85afac2f05677368b7de",
-    "url": "/MyPortfolio/static/js/2.1e8dce61.chunk.js.LICENSE"
+    "url": "/MyPortfolio/static/js/2.e512ea41.chunk.js.LICENSE"
   },
   {
-    "revision": "3b5713cc15067b02c656",
-    "url": "/MyPortfolio/static/js/main.f9668648.chunk.js"
+    "revision": "298eff60e1cfd6809c70",
+    "url": "/MyPortfolio/static/js/main.a0863b44.chunk.js"
   },
   {
     "revision": "df5514d56abd8ccddadc",
@@ -38,5 +38,9 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "61a8d063b35f6ae6482c258f8b615703",
     "url": "/MyPortfolio/static/media/logo3.61a8d063.jpg"
+  },
+  {
+    "revision": "7f66c9132eb18ec24d3e5b28fb49ab09",
+    "url": "/MyPortfolio/static/media/logo4.7f66c913.jpeg"
   }
 ]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/MyPortfolio/precache-manifest.f405b23a19ce4b2e915a5ef5c0e27c18.js"
+  "/MyPortfolio/precache-manifest.bc32c9d049c833fa2f6036322b6e15b4.js"
 );
 
 self.addEventListener('message', (event) => {
